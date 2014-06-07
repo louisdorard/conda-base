0f31aff72aef03e485d49c983488bb08382ac3f6
Louis Dorard <louis@dorard.me>
2014-06-07 23:14:19 +0200

    Use Miniconda instead of Miniconda3
    Miniconda is based on Python 2 and makes it preferable for compatibility reasons

--- instructions.sh
+++ instructions.sh
@@ -4,8 +4,8 @@ apt-get update
 # as vagrant user
 echo "alias ipynb='ipython notebook --ip=0.0.0.0 /vagrant/'" >> .bashrc
 source .bashrc
-wget http://repo.continuum.io/miniconda/Miniconda-3.3.0-Linux-x86_64.sh
-bash Miniconda3-3.3.0-Linux-x86_64.sh # accept license when prompted
-rm Miniconda-3.3.0-Linux-x86_64.sh
+wget http://repo.continuum.io/miniconda/Miniconda-3.5.2-Linux-x86_64.sh # this is the version based on Python 2
+bash Miniconda-3.5.2-Linux-x86_64.sh # accept license when prompted
+rm Miniconda-3.5.2-Linux-x86_64.sh
 conda update conda # make sure we're up to date
 conda install pandas scikit-learn ipython-notebook --yes
